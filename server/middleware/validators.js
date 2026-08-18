a0c64dff6cfdb3bcde2ca1cb8519d34aa58276fa
lordkiru <festiloamilo@gmail.com>
2026-08-18 21:33:14 +0100

    Security improvements: Strengthen staff passwords and update student form
    Option G - Strengthen Staff Passwords:
    - Require 8+ characters (was 6)
    - Require uppercase letter
    - Require lowercase letter
    - Require number
    - Require special character (@%*?&)
    
    Option B - Credentials Rotated:
    - Generated new JWT secret (64 chars)
    - Created new MongoDB user
    - Regenerated Paystack API key
    - Generated new Cloudinary credentials
    - All credentials updated locally
    
    Student Form Updates:
    - Gender limited to Male/Female only
    - Date of birth required
    - All validation active

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -98,8 +98,10 @@ const validateStaff = [
   body('password')
     .notEmpty()
     .withMessage('Password is required')
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters'),
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/)
+    .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)'),
   body('role')
     .notEmpty()
     .withMessage('Role is required')

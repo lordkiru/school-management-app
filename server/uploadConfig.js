36fd280c5d3ea2bbef996b9bd335ad7474ae3bb9
lordkiru <festiloamilo@gmail.com>
2026-08-19 11:12:04 +0100

    Fix: Update CloudinaryStorage import for multer-storage-cloudinary v2.2.1 compatibility

--- server/uploadConfig.js
+++ server/uploadConfig.js
@@ -1,8 +1,8 @@
 const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary');
 const cloudinary = require('./cloudinaryConfig');
 
-const storage = new CloudinaryStorage({
+const storage = CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'school-logos',

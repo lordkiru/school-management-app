f8aa4998f96a9554b7084fd6193e46ed98920068
lordkiru <festiloamilo@gmail.com>
2026-08-25 21:14:47 +0100

    fix: remove strict password complexity requirement for parent accounts

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -72,9 +72,7 @@ const validateParent = [
     .notEmpty()
     .withMessage('Password is required')
     .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters')
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
-    .withMessage('Password must contain at least one uppercase letter, one lowercase letter, and one number'),
+    .withMessage('Password must be at least 6 characters'),
   body('phone')
     .optional()
     .matches(/^[+]?[\d\s-()]+$/)
